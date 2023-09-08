ad7b82db2ac5b89bab0388634a309cf57930ffb7
Pinchai <>
2023-09-08 21:54:36 +0700

    no message

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import random
 
 import randomname
 
@@ -9,26 +10,58 @@ app = Flask(__name__)
 @app.route('/home')
 def home():
     filter_category = request.args.get('filter_category', default="all", type=str)
-    category = []
     products = []
+    categories = [
+        {
+            "id": 1,
+            'name': 'drink'
+        },
+        {
+            'id': 2,
+            'name': 'food'
+        },
+        {
+            'id': 3,
+            'name': 'beer'
+        },
+        {
+            'id': 4,
+            'name': 'fast food'
+        },
+        {
+            'id': 5,
+            'name': 'coffee'
+        },
+        {
+            'id': 6,
+            'name': 'tea'
+        },
+    ]
 
-    for item in range(50):
+    for item in range(12):
+        category = random.choice(categories)
         products.append({
             'id': 1,
-            'name': randomname.get_name(noun=('food', 'cats')),
-            'price': 10,
-            'discount': 40,
-            'category_name': 'drink',
+            'name': randomname.get_name(noun=('food')),
+            'price': random.randint(1, 1000),
+            'discount': random.randint(1, 100),
+            'category_name': category['name'],
         })
-    for item in range(5):
-        category.append(
-            {
-                'id': 1,
-                'name': randomname.get_name(noun=('gaming')),
-            },
-        )
 
-    return render_template('index.html', products=products, category=category, filter_category=filter_category)
+    product_filter = []
+    if filter_category == 'all' or filter_category == '':
+        product_filter = products
+    else:
+        for pro_item in products:
+            if pro_item['category_name'] == filter_category:
+                product_filter.append(pro_item)
+
+    return render_template(
+        'index.html',
+        products=product_filter,
+        category=categories,
+        filter_category=filter_category
+    )
 
 
 @app.route('/pos')
@@ -55,7 +88,6 @@ def pos_index():
 
 @app.route('/product_detail/<string:name>/<string:category>/<string:price>/<string:image>')
 def product_detail(name, category, price, image):
-
     return render_template('product_detail.html', name=name, category=category, price=price, image=image)
 
 
